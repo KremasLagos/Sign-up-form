db9465e99cc0221dafb87a25c9bd828b21540f87
KremasLagos <stathxmkourtis@gmail.com>
2025-06-25 19:40:38 +0300

    Expiriments

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './src/index.html',
+    './src/**/*.{js,css}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'norse': ['Norse', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
